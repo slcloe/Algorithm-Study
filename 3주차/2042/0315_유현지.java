cea7aa2e05ac7d8ba8741d55f159582389ce0cd7
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-03-15 23:39:42 +0900

    3주차 구간 합 구하기 solved

--- 3주차/2042/0315_유현지.java
+++ 3주차/2042/0315_유현지.java
@@ -1,9 +1,22 @@
-package a2403;
+package a2403.week2;
+
+/*
+ * | 활용 자료구조 | Segment Tree
+ *   참고 링크 : https://blog.naver.com/ndb796/221282210534
+ * | 활용 알고리즘 | Binary Search
+ * | 접근 방법 |
+ *  1. 1~N의 합을 가진 루트 노드의 왼쪽 자식 노드는 1~N/2의 합, 오른쪽 자식 노드는 N/2+1~N의 합 ...
+ *  2. 위의 규칙에 따라 재귀적으로 세그먼트 트리를 만든다
+ *     * 세그먼트 트리의 크기 = 2^0 + 2^1 + 2^2 + ... + 2^X
+ *       (2^X: N 이상이면서 가장 작은 2의 제곱수)
+ *  3. 수의 갱신이 일어나면 세그먼트 트리에서 해당 수의 리프 노드에서부터 루트 노드까지
+ *     모든 노드에서 기존 수와 변경할 수의 차를 더하며 세그먼트 트리를 갱신한다
+ */
 
 import java.io.*;
 import java.util.*;
 
-public class d15_bj_g1_2042_구간_합_구하기 {
+public class bj_g1_2042_구간_합_구하기 {
     static long[] arr, segTree;
 
     public static void main(String[] args) throws Exception{
