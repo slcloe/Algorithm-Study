d255f8e1aa1330b10499fd7847af98857b1735ec
신종욱 <rmm1@naver.com>
2024-03-29 23:54:48 +0900

    5주차 미친 아두이노 solved

---
+++ 5주차/2151/0329_신종욱.py
@@ -0,0 +1,50 @@
+import sys
+input = sys.__stdin__.readline
+
+from collections import deque
+
+def main():
+    n = int(input())
+
+    house = []
+    door_loc = []
+    visited = [[1000] * n for _ in range(n)]
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+
+
+    for i in range(n):
+        house.append(list(input().rstrip()))
+        for j in range(n):
+            if house[i][j] == '#':
+                door_loc.append((i, j))
+
+    x1, y1, x2, y2 = door_loc[0][0], door_loc[0][1], door_loc[1][0], door_loc[1][1]
+    queue = deque([(door_loc[0], 0, direction) for direction in range(4)])
+
+    while queue:
+        (cx, cy), mirror_count, direction = queue.popleft()
+        
+        nx = cx + dx[direction]
+        ny = cy + dy[direction]
+            
+        while (0 <= nx < n) and (0 <= ny < n) and (house[nx][ny] != '*'):
+            if house[nx][ny] == "!":
+                if direction == 0 or direction == 2:
+                    queue.append(((nx,ny), mirror_count + 1, 1))
+                    queue.append(((nx,ny), mirror_count + 1, 3))
+                else:
+                    queue.append(((nx,ny), mirror_count + 1, 0))
+                    queue.append(((nx,ny), mirror_count + 1, 2))
+
+            if nx == x2 and ny == y2:
+                queue.clear()
+                break
+
+            nx += dx[direction]
+            ny += dy[direction]        
+
+    print(mirror_count)
+
+
+main()
