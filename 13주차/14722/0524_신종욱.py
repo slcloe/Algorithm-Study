68cb64f6fa9b2a139d38b8a122a627737444e7fe
Vol de nuit <71578653+beyondthemist@users.noreply.github.com>
2024-05-24 15:43:07 +0900

    13주차 우유 도시 solved

--- 13주차/14722/0524_신종욱.py
+++ 13주차/14722/0524_신종욱.py
@@ -4,9 +4,7 @@ input = sys.__stdin__.readline
 def main(): 
     n = int(input())
     city = [list(map(int, input().split())) for _ in range(n)]
-
     dp = [[0 for j in range(n + 1)] for i in range(n + 1)]
-    dp[0][0] = int(city[0][0] == 0)
 
     for i in range(1, n + 1):
         for j in range(1, n + 1):
