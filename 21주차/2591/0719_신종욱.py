7fc234d6b89bb0eccc32613e7fdc691c6c7b0bde
신종욱 <71578653+beyondthemist@users.noreply.github.com>
2024-07-19 18:13:13 +0900

    21주차 숫자카드 solved

--- 21주차/2591/0719_신종욱.py
+++ 21주차/2591/0719_신종욱.py
@@ -10,17 +10,16 @@ def main():
     # dp[i]= 1 ~ i번째 숫자로 만들 수 있는 모든 경우의 수
     dp = [1 for _ in range(n)]
 
-
     for i in range(1, n):
         # 둘 다 가능한 경우
         if (digits[i - 1] != 0) and (digits[i] != 0) and (digits[i - 1]*10) + digits[i] <= 34:
             dp[i] = dp[i - 1] + dp[i - 2]
             continue
-        
+
         # 한 자리 수만 가능한 경우
         elif digits[i - 1] == 0 or (digits[i - 1]*10) + digits[i] > 34:
             dp[i] = dp[i - 1]
-        
+
         # 두 자리 수만 가능한 경우
         else:
             # digits[i - 1]과 digits[i]를 합치므로
@@ -28,5 +27,6 @@ def main():
             dp[i] = dp[i - 2]
 
     print(dp[-1])
+
 
 main()
